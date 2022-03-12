25326766fad41c0087db867ccb779f8ffac8d56d
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-13 01:51:51 +0700

    Update start.py

--- userbot/utils/start.py
+++ userbot/utils/start.py
@@ -11,7 +11,7 @@ async def startupmessage():
         if BOTLOG:
             await tgbot.send_file(
                 BOTLOG_CHATID,
-                "https://telegra.ph/file/a7915ed28a022aa9125a6.jpg",
+                "https://telegra.ph/file/8dad04830561de07c35d7.jpg",
                 caption="⚡ **AbingxUserbot Berhasil Diaktifkan**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 8.0@AbingxUserbot\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @AbingProject ",
                 buttons=[(Button.url("ꜱᴜᴘᴘᴏʀᴛ", "https://t.me/AbingSupport"),)],
             )
