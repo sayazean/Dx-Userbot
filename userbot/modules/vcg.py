14f6cbd153a6b6d0f3220575579eb44fb986a7c6
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-14 13:28:23 +0700

    Create vcg.py

---
+++ userbot/modules/vcg.py
@@ -0,0 +1,94 @@
+# Thanks Full To Team Ultroid
+# Fiks By ABING @SayaAbing
+
+
+from telethon.tl.functions.channels import GetFullChannelRequest as getchat
+from telethon.tl.functions.phone import CreateGroupCallRequest as startvc
+from telethon.tl.functions.phone import DiscardGroupCallRequest as stopvc
+from telethon.tl.functions.phone import GetGroupCallRequest as getvc
+from telethon.tl.functions.phone import InviteToGroupCallRequest as invitetovc
+
+from userbot import CMD_HELP, CMD_HANDLER as cmd
+from userbot.utils import kyy_cmd
+
+NO_ADMIN = "`Maaf Kamu Bukan Admin 👮`"
+
+
+def vcmention(user):
+    full_name = get_display_name(user)
+    if not isinstance(user, types.User):
+        return full_name
+    return f"[{full_name}](tg://user?id={user.id})"
+
+
+async def get_call(kyy):
+    kyy = await kyy.client(getchat(kyy.chat_id))
+    await kyy.client(getvc(kyy.full_chat.call, limit=1))
+    return hehe.call
+
+
+def user_list(l, n):
+    for i in range(0, len(l), n):
+        yield l[i: i + n]
+
+
+@kyy_cmd(pattern="startvc$")
+async def start_voice(c):
+    chat = await c.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    if not admin and not creator:
+        await c.edit(f"**Maaf {ALIVE_NAME} Bukan Admin 👮**")
+        return
+    try:
+        await c.client(startvc(c.chat_id))
+        await c.edit("`Memulai Obrolan Suara`")
+    except Exception as ex:
+        await c.edit(f"**ERROR:** `{ex}`")
+
+
+@kyy_cmd(pattern="stopvc$")
+async def stop_voice(c):
+    chat = await c.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    if not admin and not creator:
+        await c.edit(f"**Maaf {ALIVE_NAME} Bukan Admin 👮**")
+        return
+    try:
+        await c.client(stopvc(await get_call(c)))
+        await c.edit("`Mematikan Obrolan Suara`")
+    except Exception as ex:
+        await c.edit(f"**ERROR:** `{ex}`")
+
+
+@kyy_cmd(pattern="vcinvite")
+async def _(kyy):
+    await kyy.edit("`Sedang Menginvite Member...`")
+    users = []
+    z = 0
+    async for x in kyy.client.iter_participants(kyy.chat_id):
+        if not x.bot:
+            users.append(x.id)
+    hmm = list(user_list(users, 6))
+    for p in hmm:
+        try:
+            await kyy.client(invitetovc(call=await get_call(kyy), users=p))
+            z += 6
+        except BaseException:
+            pass
+    await kyy.edit(f"`Menginvite {z} Member`")
+
+
+CMD_HELP.update(
+    {
+        "vcg": f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}startvc`\
+         \n↳ : Memulai Obrolan Suara dalam Group.\
+         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}stopvc`\
+         \n↳ : `Menghentikan Obrolan Suara Pada Group.`\
+         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}vcinvite`\
+         \n↳ : Invite semua member yang berada di group."
+    }
+)
