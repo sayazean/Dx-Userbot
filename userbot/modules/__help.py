98f99b5607d1ee4d05da7379a8cdefbec900d525
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-17 21:36:13 +0700

    Update __help.py

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -20,14 +20,14 @@
 
 
 from userbot import BOT_USERNAME, CMD_HELP, bot
-from userbot.utils import edit_or_reply, edit_delete, kyy_cmd
+from userbot.utils import edit_or_reply, edit_delete, bing_cmd
 
 user = bot.get_me()
 DEFAULTUSER = user.first_name
 CUSTOM_HELP_EMOJI = "⚡"
 
 
-@kyy_cmd(pattern="help ?(.*)")
+@bing_cmd(pattern="help ?(.*)")
 async def cmd_list(event):
     args = event.pattern_match.group(1).lower()
     if args:
