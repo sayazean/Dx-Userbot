a476bdf09fbd7eda830732a577797666100f915b
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-17 15:14:42 +0700

    Update __help.py

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -32,7 +32,7 @@ async def cmd_list(event):
     args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
-            await edit_or_reply(event, f"**✘ Commands available in {args} ✘** \n\n" + str(CMD_HELP[args]) + "\n\n**☞ @AbingProject**")
+            await edit_or_reply(event, f"**𑁍 Commands available in {args} 𑁍** \n\n" + str(CMD_HELP[args]) + "\n\n**☞ @AbingProject**")
         else:
             await edit_delete(event, f"**Module** `{args}` **Tidak tersedia!**")
     else:
