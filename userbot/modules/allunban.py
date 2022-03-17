02b6602b7f200b5d6a233c1d390f0196e8fc09d2
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-17 13:50:34 +0700

    Update allunban.py

--- userbot/modules/allunban.py
+++ userbot/modules/allunban.py
@@ -1,16 +1,14 @@
-# Port By @VckyouuBitch From GeezProjects
-# Copyright © 2021 Geez-Projects
 from telethon.tl.types import (
     ChannelParticipantsKicked,
 )
 
-from userbot.events import register
-from userbot import CMD_HELP
+from userbot.utils import edit_or_reply, kyy_cmd
+from userbot import CMD_HELP, CMD_HANDLER as cmd
 
 
-@register(outgoing=True, pattern=r"^\.allunban(?: |$)(.*)", groups_only=True)
+@kyy_cmd(pattern="allunban(?: |$)(.*)")
 async def _(event):
-    await event.edit("`Sedang Mencari List Banning.`")
+    xx = await edit_or_reply(event, "`Sedang Mencari List Banning.`")
     p = 0
     (await event.get_chat()).title
     async for i in event.client.iter_participants(
@@ -23,12 +21,12 @@ async def _(event):
             p += 1
         except BaseException:
             pass
-    await event.edit("`Sukses Menghapus List Banning`")
+    await xx.edit("`Sukses Menghapus List Banning`")
 
 
 CMD_HELP.update(
     {
-        "allunban": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.allunban`\
+        "allunban": f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}allunban`\
     \n↳ : Membatalkan semua Ban Di Anggota Grup."
     }
 )
