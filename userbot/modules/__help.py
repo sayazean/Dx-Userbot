7def350b16aa66cc817769915f5690bdcb2e1574
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-17 18:09:54 +0700

    Update __help.py

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -24,15 +24,14 @@ from userbot.utils import edit_or_reply, edit_delete, kyy_cmd
 
 user = bot.get_me()
 DEFAULTUSER = user.first_name
-CUSTOM_HELP_EMOJI = "✨"
-
+CUSTOM_HELP_EMOJI = "⚡"
 
 @kyy_cmd(pattern="help ?(.*)")
 async def cmd_list(event):
     args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
-            await edit_or_reply(event, f"**✘ Commands available in {args} ✘** \n\n" + str(CMD_HELP[args]) + "\n\n**☞ @NastyProject**")
+            await edit_or_reply(event, f"**✘ Commands available in {args} ✘** \n\n" + str(CMD_HELP[args]) + "\n\n**☞ @AbingProject**")
         else:
             await edit_delete(event, f"**Module** `{args}` **Tidak tersedia!**")
     else:
