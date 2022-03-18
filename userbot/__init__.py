6e97cef35e414822da21cc3403edf4bdf840a9a4
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-18 16:23:21 +0700

    Hihihuahauaha

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -457,7 +457,7 @@ with bot:
         bot.loop.run_until_complete(checking())
     except BaseException:
         LOGS.info(
-            "Join Support Group @AbingSupportt and Channel @AbingProjectt to see the updates of userbot"
+            "Join Support Group @AbingSupport and Channel @AbingProject to see the updates of userbot"
             "Don't Leave")
         quit(1)
 
