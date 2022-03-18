7e4c20cef41b36d236fc23e039fd425d5e95d09e
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-18 12:40:59 +0700

    Update aesthetic.py

--- userbot/modules/aesthetic.py
+++ userbot/modules/aesthetic.py
@@ -1,5 +1,5 @@
 from telethon import events
-from userbot.utils import kyy_cmd
+from userbot.utils import bing_cmd
 from userbot import CMD_HELP, CMD_HANDLER as cmd
 
 PRINTABLE_ASCII = range(0x21, 0x7F)
@@ -15,7 +15,7 @@ def aesthetify(string):
         yield chr(c)
 
 
-@kyy_cmd(pattern="ae(?: |$)(.*)")
+@bing_cmd(pattern="ae(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
