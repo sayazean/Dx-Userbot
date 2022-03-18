3a9a5556f1b8f99df1ab260da136769550d16fc5
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-18 13:55:09 +0700

    Update abinghelper.py

--- userbot/modules/abinghelper.py
+++ userbot/modules/abinghelper.py
@@ -1,33 +1,28 @@
 """ Userbot module for other small commands. """
-from userbot import CMD_HELP, ALIVE_NAME
-from userbot.events import register
+from userbot import CMD_HELP, owner, CMD_HANDLER as cmd
+from userbot.utils import edit_or_reply, bing_cmd
 
 
-# ================= CONSTANT =================
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
-# ============================================
-
-
-@register(outgoing=True, pattern="^.lhelp$")
+@bing_cmd(pattern="lhelp$")
 async def usit(e):
-    await e.edit(
-        f"**Halo {DEFAULTUSER} Jika Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke @sayaabing atau tag admin di @AbingSupport:\n"
-        "\n[Telegram](t.me/sayaabing)"
-        "\n[Repo](https://github.com/SayaAbing/AbingxUserbot/)"
-        "\n[Instagram](instagram.com/aabinggg/)")
+    await edit_or_reply(e,
+                        f"**Halo {owner} Jika Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `{cmd}help` Atau Bisa Minta Bantuan Ke:\n"
+                        "\n[Telegram](t.me/sayaabing)"
+                        "\n[Repo](https://github.com/S/AbingxUserbot)"
+                        "\n[Instagram](instagram.com/aabinggg)")
 
 
-@register(outgoing=True, pattern="^.vars$")
+@bing_cmd(pattern="vars$")
 async def var(m):
-    await m.edit(
-        f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/Yansaii/Bdrl-userbot/Bdrl-userbot/varshelper.txt)")
+    await edit_or_reply(m,
+                        f"**Disini Daftar Vars Dari {owner}:**\n"
+                        "\n[DAFTAR VARS](https://raw.githubusercontent.com/SayaAbing/AbingxUserbot/AbingxUserbot/varshelper.txt)")
 
 
 CMD_HELP.update({
     "helper":
-    "`.lhelp`\
+    f"`{cmd}lhelp`\
 \nUsage: Bantuan Untuk AbingxUserbot.\
-\n`.vars`\
+\n`{cmd}vars`\
 \nUsage: Melihat Daftar Vars."
 })
