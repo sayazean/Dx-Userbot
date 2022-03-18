bd9bc894f2568b476e31e7958b4897a211553d34
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-18 15:21:47 +0700

    hmmmm

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -599,7 +599,7 @@ with bot:
         uid = user.id
         owner = user.first_name
         logo = ALIVE_LOGO
-        binglogo = INLINE_PIC
+        kyylogo = INLINE_PIC
         tgbotusername = BOT_USERNAME
         BTN_URL_REGEX = re.compile(
             r"(\[([^\[]+?)\]\<buttonurl:(?:/{0,2})(.+?)(:same)?\>)"
@@ -614,7 +614,7 @@ with bot:
                 text = f"**⚡️ ᴀʙɪɴɢxυѕєявσт ɪɴʟɪɴᴇ ᴍᴇɴᴜ ⚡️**\n\n✣ **ᴏᴡɴᴇʀ** [{user.first_name}](tg://user?id={user.id})\n✣ **ᴊᴜᴍʟᴀʜ** `{len(dugmeler)}` **Modules**"
                 await event.edit(
                     text,
-                    file=binglogo,
+                    file=kyylogo,
                     buttons=buttons,
                     link_preview=False,
                 )
@@ -701,7 +701,7 @@ with bot:
             if event.query.user_id == uid and query.startswith("@vuserrrbot"):
                 buttons = paginate_help(0, dugmeler, "helpme")
                 result = builder.photo(
-                    file=binglogo,
+                    file=kyylogo,
                     link_preview=False,
                     text=f"**⚡️ ᴀʙɪɴɢxυѕєявσт ɪɴʟɪɴᴇ ᴍᴇɴᴜ ⚡️**\n\n✣ **ᴏᴡɴᴇʀ** [{user.first_name}](tg://user?id={user.id})\n✣ **ᴊᴜᴍʟᴀʜ** `{len(dugmeler)}` **Modules**",
                     buttons=buttons,
