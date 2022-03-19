cbea2201bfc2387d7763c08a665498dd84867a11
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-19 17:21:59 +0700

    Update deteksigrub.py

--- userbot/modules/deteksigrub.py
+++ userbot/modules/deteksigrub.py
@@ -1,16 +1,16 @@
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from userbot import CMD_HELP, bot
-from userbot.events import register
+from userbot import CMD_HELP, bot, CMD_HANDLER as cmd
+from userbot.utils import edit_or_reply, edit_delete, bing_cmd
 
 
-@register(outgoing=True, pattern=r"^\.(?:dgrup|dg)\s?(.*)?")
+@bing_cmd(pattern="(?:dgrup|dg)\\s?(.*)?")
 async def _(event):
     if event.fwd_from:
         return
     input_str = "".join(event.text.split(maxsplit=1)[1:])
     reply_message = await event.get_reply_message()
     if not event.reply_to_msg_id:
-        await event.edit("```Mohon Balas Ke Pesan Pengguna atau ketik .dgrup (ID/Username) Yang mau Anda deteksi```")
+        await edit_delete(event, "```Mohon Balas Ke Pesan Pengguna atau ketik .dgrup (ID/Username) Yang mau Anda deteksi```")
         return
     if input_str:
         try:
@@ -19,13 +19,13 @@ async def _(event):
             try:
                 u = await event.client.get_entity(input_str)
             except ValueError:
-                await edit.event("`Mohon Berikan ID/Username untuk menemukan Riwayat`"
-                                 )
+                await edit_delete(event, "`Mohon Berikan ID/Username untuk menemukan Riwayat`"
+                                  )
             uid = u.id
     else:
         uid = reply_message.sender_id
     chat = "@tgscanrobot"
-    event = await event.edit("`Sedang Mendeteksi...`")
+    xx = await edit_or_reply(event, "`Sedang Mendeteksi...`")
     async with bot.conversation(chat) as conv:
         try:
             await conv.send_message(f"{uid}")
@@ -35,7 +35,7 @@ async def _(event):
             )
         response = await conv.get_response()
         await event.client.send_read_acknowledge(conv.chat_id)
-        await event.edit(response.text)
+        await xx.edit(response.text)
 
 
 def inline_mention(user):
@@ -51,6 +51,6 @@ def user_full_name(user):
 
 CMD_HELP.update({
     "deteksi":
-        "`.dgrup` ; `.dg`\
+        f"`{cmd}dgrup` ; `{cmd}dg`\
     \nPenjelasan: Melihat Riwayat Grup Yang Pernah / Sedang dimasuki."
 })
