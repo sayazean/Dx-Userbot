b8f0d2524d38b633b9f91677a4f098c5ad2c3d58
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-19 17:33:40 +0700

    Update fakeAction.py

--- userbot/modules/fakeAction.py
+++ userbot/modules/fakeAction.py
@@ -1,11 +1,12 @@
 # Port By @IDnyaKosong From Kyy-Userbot
 # # Copyright (C) 2021 Kyy-Userbot
-from userbot.events import register
-from userbot import CMD_HELP
+
+from userbot.utils import edit_or_reply, edit_delete, bing_cmd
+from userbot import CMD_HELP, CMD_HANDLER as cmd
 import asyncio
 
 
-@register(outgoing=True, pattern="^.ftyping(?: |$)(.*)")
+@bing_cmd(pattern="ftyping(?: |$)(.*)")
 async def _(event):
     t = event.pattern_match.group(1)
     if not (t or t.isdigit()):
@@ -17,13 +18,13 @@ async def _(event):
             try:
                 t = await event.ban_time(t)
             except BaseException:
-                return await event.edit("`Incorrect Format`")
-    await event.edit(f"`Memulai Pengetikan Palsu Selama {t} sec.`")
+                return await edit_delete(event, "`Incorrect Format`")
+    await edit_or_reply(event, f"`Memulai Pengetikan Palsu Selama {t} sec.`")
     async with event.client.action(event.chat_id, "typing"):
         await asyncio.sleep(t)
 
 
-@register(outgoing=True, pattern="^.faudio(?: |$)(.*)")
+@bing_cmd(pattern="faudio(?: |$)(.*)")
 async def _(event):
     t = event.pattern_match.group(1)
     if not (t or t.isdigit()):
@@ -35,13 +36,13 @@ async def _(event):
             try:
                 t = await event.ban_time(t)
             except BaseException:
-                return await event.edit("`Incorrect Format`")
-    await event.edit(f"`Memulai merekam audio palsu Selama {t} sec.`")
+                return await edit_delete(event, "`Incorrect Format`")
+    await edit_or_reply(event, f"`Memulai merekam audio palsu Selama {t} sec.`")
     async with event.client.action(event.chat_id, "record-audio"):
         await asyncio.sleep(t)
 
 
-@register(outgoing=True, pattern="^.fvideo(?: |$)(.*)")
+@bing_cmd(pattern="fvideo(?: |$)(.*)")
 async def _(event):
     t = event.pattern_match.group(1)
     if not (t or t.isdigit()):
@@ -53,13 +54,13 @@ async def _(event):
             try:
                 t = await event.ban_time(t)
             except BaseException:
-                return await event.edit("`Incorrect Format`")
-    await event.edit(f"`Memulai merekam video palsu selama {t} sec.`")
+                return await edit_delete(event, "`Incorrect Format`")
+    await edit_or_reply(event, f"`Memulai merekam video palsu selama {t} sec.`")
     async with event.client.action(event.chat_id, "record-video"):
         await asyncio.sleep(t)
 
 
-@register(outgoing=True, pattern="^.fgame(?: |$)(.*)")
+@bing_cmd(pattern="fgame(?: |$)(.*)")
 async def _(event):
     t = event.pattern_match.group(1)
     if not (t or t.isdigit()):
@@ -71,13 +72,13 @@ async def _(event):
             try:
                 t = await event.ban_time(t)
             except BaseException:
-                return await event.edit("`Incorrect Format`")
-    await event.edit(f"`Memulai Bermain Game Palsu Selama {t} sec.`")
+                return await edit_delete(event, "`Incorrect Format`")
+    await edit_or_reply(event, f"`Memulai Bermain Game Palsu Selama {t} sec.`")
     async with event.client.action(event.chat_id, "game"):
         await asyncio.sleep(t)
 
 
-@register(outgoing=True, pattern="^.fround(?: |$)(.*)")
+@bing_cmd(pattern="fround(?: |$)(.*)")
 async def _(event):
     t = event.pattern_match.group(1)
     if not (t or t.isdigit()):
@@ -89,15 +90,15 @@ async def _(event):
             try:
                 t = await event.ban_time(t)
             except BaseException:
-                return await event.edit("`Masukan jumlah detik yang benar`")
-    await event.edit(f"`Memulai merekam video message palsu Selama {t} sec.`")
+                return await edit_delete(event, "`Masukan jumlah detik yang benar`")
+    xx = await edit_delete(event, f"`Memulai merekam video message palsu Selama {t} sec.`")
     await asyncio.sleep(3)
-    await event.delete()
+    await xx.delete()
     async with event.client.action(event.chat_id, "record-round"):
         await asyncio.sleep(t)
 
 
-@register(outgoing=True, pattern="^.fphoto(?: |$)(.*)")
+@bing_cmd(pattern="fphoto(?: |$)(.*)")
 async def _(event):
     t = event.pattern_match.group(1)
     if not (t or t.isdigit()):
@@ -109,15 +110,15 @@ async def _(event):
             try:
                 t = await event.ban_time(t)
             except BaseException:
-                return await event.edit("`Masukan jumlah detik yang benar`")
-    await event.edit(f"`Memulai Mengirim Photo Palsu Selama` {t} sec.`")
+                return await edit_delete(event, "`Masukan jumlah detik yang benar`")
+    xx = await edit_or_reply(event, f"`Memulai Mengirim Photo Palsu Selama` {t} sec.`")
     await asyncio.sleep(3)
-    await event.delete()
+    await xx.delete()
     async with event.client.action(event.chat_id, "photo"):
         await asyncio.sleep(t)
 
 
-@register(outgoing=True, pattern="^.fdocument(?: |$)(.*)")
+@bing_cmd(pattern="fdocument(?: |$)(.*)")
 async def _(event):
     t = event.pattern_match.group(1)
     if not (t or t.isdigit()):
@@ -129,15 +130,16 @@ async def _(event):
             try:
                 t = await event.ban_time(t)
             except BaseException:
-                return await event.edit("`Masukan jumlah detik yang benar`")
-    await event.edit(f"`Memulai Mengirim Document Palsu Selama` {t} sec.`")
+                return await edit_delete(event, "`Masukan jumlah detik yang benar`")
+    xx = edit_or_reply(
+        event, f"`Memulai Mengirim Document Palsu Selama` {t} sec.`")
     await asyncio.sleep(3)
-    await event.delete()
+    await xx.delete()
     async with event.client.action(event.chat_id, "document"):
         await asyncio.sleep(t)
 
 
-@register(outgoing=True, pattern="^.flocation(?: |$)(.*)")
+@bing_cmd(pattern="flocation(?: |$)(.*)")
 async def _(event):
     t = event.pattern_match.group(1)
     if not (t or t.isdigit()):
@@ -149,15 +151,15 @@ async def _(event):
             try:
                 t = await event.ban_time(t)
             except BaseException:
-                return await event.edit("`Masukan jumlah detik yang benar`")
-    await event.edit(f"`Memulai Share Lokasi Palsu Selama` {t} sec.`")
+                return await edit_delete(event, "`Masukan jumlah detik yang benar`")
+    xx = await edit_or_reply(event, f"`Memulai Share Lokasi Palsu Selama` {t} sec.`")
     await asyncio.sleep(3)
-    await event.delete()
+    await xx.delete()
     async with event.client.action(event.chat_id, "location"):
         await asyncio.sleep(t)
 
 
-@register(outgoing=True, pattern="^.fcontact(?: |$)(.*)")
+@bing_cmd(pattern="fcontact(?: |$)(.*)")
 async def _(event):
     t = event.pattern_match.group(1)
     if not (t or t.isdigit()):
@@ -169,30 +171,30 @@ async def _(event):
             try:
                 t = await event.ban_time(t)
             except BaseException:
-                return await event.edit("`Masukan jumlah detik yang benar`")
-    await event.edit(f"`Memulai Mengirim Contact Palsu Selama` {t} sec.`")
+                return await edit_delete(event, "`Masukan jumlah detik yang benar`")
+    xx = await edit_or_reply(event, f"`Memulai Mengirim Contact Palsu Selama` {t} sec.`")
     await asyncio.sleep(3)
-    await event.delete()
+    await xx.delete()
     async with event.client.action(event.chat_id, "contact"):
         await asyncio.sleep(t)
 
 
 CMD_HELP.update({
     "fakeaction":
-    "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.ftyping` <jumlah teks>\
+    f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}ftyping` <jumlah teks>\
    \nUsage : Seakan akan sedang mengetik padahal tidak\
-   \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.faudio` <jumlah teks>\
+   \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}faudio` <jumlah teks>\
    \nUsage : Berfungsi sama seperti ftyping tapi ini dalam bentuk fake audio\
-   \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.fvideo` <jumlah teks>\
+   \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}video` <jumlah teks>\
    \nUsage : Berfungsi sama seperti ftyping tapi ini dalam bentuk fake video\
-   \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.fgame` <jumlah teks>\
+   \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}fgame` <jumlah teks>\
    \nUsage : Berfungsi sama seperti ftyping tapi ini dalam bentuk fake game\
-   \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.fphoto` <jumlah teks>\
+   \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}fphoto` <jumlah teks>\
    \nUsage : Berfungsi sama seperti ftyping tapi ini dalam bentuk fake foto\
-   \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.fdocument` <jumlah teks>\
+   \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}fdocument` <jumlah teks>\
    \nUsage : Berfungsi sama seperti ftyping tapi ini dalam bentuk fake document\
-   \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.flocation` <jumlah teks>\
+   \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}flocation` <jumlah teks>\
    \nUsage : Berfungsi sama seperti ftyping tapi ini dalam bentuk fake location\
-   \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.fcontact` <jumlah teks>\
+   \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}fcontact` <jumlah teks>\
    \nUsage : Berfungsi sama seperti ftyping tapi ini dalam bentuk fake contact"
 })
