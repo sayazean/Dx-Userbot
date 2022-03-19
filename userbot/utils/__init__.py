1867b1affc2b1dc8d47b6480276c0685058b007b
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-19 14:32:45 +0700

    Update __init__.py

--- userbot/utils/__init__.py
+++ userbot/utils/__init__.py
@@ -17,7 +17,7 @@ from .start import startupmessage
 from . import format as _format
 from .format import parse_pre
 from .chrome import chrome, options
-from .decorator import asst_cmd, callback, abing_cmd, abing_handler
+from .decorator import asst_cmd, callback, bing_cmd, bing_handler
 from .google_images_download import googleimagesdownload
 from .progress import progress, CancelProcess
 from .tools import (
