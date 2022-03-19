7e9ae1be3951d6fd6cab80f0ac2e81fe4c756151
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-19 19:07:28 +0700

    Update stickerspam.py

--- userbot/modules/stickerspam.py
+++ userbot/modules/stickerspam.py
@@ -10,26 +10,26 @@ from telethon.tl import functions, types
 from telethon.tl.functions.messages import GetStickerSetRequest
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 
-from userbot.events import register
-from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
+from userbot.utils import edit_or_reply, edit_delete, bing_cmd
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, CMD_HANDLER as cmd
 
 
-@register(outgoing=True, pattern=r"^\.sspam(?: |$)(.*)")
+@bing_cmd(pattern="sspam(?: |$)(.*)")
 async def stickerpack_spam(event):
     if event.fwd_from:
         return
     reply = await event.get_reply_message()
     if not reply or media_type(
             reply) is None or media_type(reply) != "Sticker":
-        return await event.edit("`reply to any sticker to send all stickers in that pack`"
-                                )
+        return await edit_delete(event, "`reply to any sticker to send all stickers in that pack`"
+                                 )
     hmm = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
     try:
         stickerset_attr = reply.document.attributes[1]
-        geez = await event.edit("`Fetching details of the sticker pack, please wait..`"
-                                )
+        bing = await edit_or_reply(event, "`Fetching details of the sticker pack, please wait..`"
+                                  )
     except BaseException:
-        await event.edit("`This is not a sticker. Reply to a sticker.`", 5)
+        await bing.edit("`This is not a sticker. Reply to a sticker.`", 5)
         return
     try:
         get_stickerset = await event.client(
@@ -41,8 +41,8 @@ async def stickerpack_spam(event):
             )
         )
     except Exception:
-        return await geez.edit("`I guess this sticker is not part of any pack so i cant kang this sticker pack try kang for this sticker`",
-                               )
+        return await bing.edit("`I guess this sticker is not part of any pack so i cant kang this sticker pack try kang for this sticker`",
+                              )
     try:
         hmm = Get(hmm)
         await event.client(hmm)
@@ -74,10 +74,7 @@ async def stickerpack_spam(event):
         await event.client.send_file(BOTLOG_CHATID, reqd_sticker_set.documents[0])
 
 
-CMD_HELP.update(
-    {
-        "sspam": "**Plugin : Sticker Pack Spam**\
-        \n\n**Command  :** `.sspam`\
-        \n**Usage :** `Balas ke sticker, Fungsi Spam Satu Pack.`"
-    }
-)
+CMD_HELP.update({
+    "sspam": f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}sspam`\
+        \n↳ : Balas ke sticker, Fungsi Spam Satu Pack."
+})
