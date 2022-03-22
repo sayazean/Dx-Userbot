ab0c3d79cd5287238af981d479ddcffe1b36694f
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-22 15:49:21 +0700

    typo tod babi

--- userbot/utils/__init__.py
+++ userbot/utils/__init__.py
@@ -32,7 +32,7 @@ from .tools import (
     runcmd,
     edit_delete,
     edit_or_reply,
-    edit_set,
+    text_set,
     media_type,
 )
 from .utils import autobot, load_module, remove_plugin, start_assistant, create_supergroup
