6937df84bdb1223a8e156f3e03f0f2191632cdca
ABING <nabilrmdaan3@gmail.com>
2022-03-22 08:45:16 +0000

    pylint: auto fixes
    Signed-off-by: ABING <nabilrmdaan3@gmail.com>

--- userbot/modules/nulis.py
+++ userbot/modules/nulis.py
@@ -5,7 +5,7 @@
 import os
 
 from PIL import Image, ImageDraw, ImageFont
-from userbot.utils import bing_cmd, edit_or_replay, edit_delete, text_set
+from userbot.utils import bing_cmd, edit_delete, text_set
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
 
