b05a9d85b5fc3fd6abfe11f38695bc6f70050363
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-03-30 18:53:01 +0700

    Fixessss ID YAA KNTL

--- userbot/modules/get_user_id.py
+++ userbot/modules/get_user_id.py
@@ -26,7 +26,7 @@ async def _(event):
             response = conv.wait_event(
                 events.NewMessage(
                     incoming=True,
-                    from_users=1663258664))
+                    from_users=1337194042))
             await bot.forward_messages(chat, reply_message)
             response = await response
         except YouBlockedUserError:
