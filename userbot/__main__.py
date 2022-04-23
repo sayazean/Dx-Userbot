d74393ec66ea7de0c6cd52aa4fe79a0a99e3e60a
AbinG <101431998+SayaAbing@users.noreply.github.com>
2022-04-24 05:52:36 +0700

    haha hayu jmbt

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -7,35 +7,24 @@
 
 import sys
 from importlib import import_module
+from pytgcalls import idle
 
-import requests
-from telethon.tl.functions.channels import InviteToChannelRequest as Addbot
 from userbot import (
     BOTLOG_CHATID,
-    BOT_USERNAME,
     BOT_TOKEN,
     BOT_VER,
     LOGS,
-    ALIVE_NAME,
-    abingblacklist,
     bot,
-    call_py,
 )
 from userbot.modules import ALL_MODULES
-from userbot.utils import autobot, autopilot
+from userbot.clients import abing_ubot_on, multiclientabing
+from userbot.utils import autobot, autopilot, git
 
 try:
-    bot.start()
-    call_py.start()
-    user = bot.get_me()
-    abingblacklist = requests.get(
-        "https://raw.githubusercontent.com/SayaAbing/AbingBlacklist/master/abingblacklist.json"
-    ).json()
-    if user.id in abingblacklist:
-        LOGS.warning(
-            "MAKANYA GA USAH BERTINGKAH GOBLOK, USERBOTnya GUA MATIIN NAJIS BANGET DIPAKE ORANG KEK LU.\nCredits: @moonscrsh"
-        )
-        sys.exit(1)
+    client = multiclientabing()
+    total = 5 - client
+    git()
+    LOGS.info(f"Total Clients = {total} User")
 except Exception as e:
     LOGS.info(str(e), exc_info=True)
     sys.exit(1)
@@ -43,36 +32,19 @@ except Exception as e:
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
+bot.loop.run_until_complete(abing_ubot_on())
 if not BOTLOG_CHATID:
-    LOGS.info(
-        "BOTLOG_CHATID Vars tidak terisi, Memulai Membuat Grup Otomatis..."
-    )
     bot.loop.run_until_complete(autopilot())
-
-LOGS.info(
-    f"Jika {ALIVE_NAME} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/AbingSupport")
-LOGS.info(
-    f"⚡️AbingxUserbot⚡️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
-
-
-async def check_alive():
-    try:
-        if BOTLOG_CHATID != 0:
-            await bot.send_file(BOTLOG_CHATID, ALIVE_LOGO, caption=f"⚡️**Abing x Userbot Berhasil Diaktifkan**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 3.1.5@AbingxUserbot\\ **Ketik** `.ping` **Untuk Mengecheck Bot**\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @AbingProject ")
-    except Exception as e:
-        LOGS.info(str(e))
-    try:
-        await bot(Addbot(int(BOTLOG_CHATID), [BOT_USERNAME]))
-    except BaseException:
-        pass
-
-bot.loop.run_until_complete(check_alive())
 if not BOT_TOKEN:
     LOGS.info(
         "BOT_TOKEN Vars tidak terisi, Memulai Membuat BOT Otomatis di @Botfather..."
     )
     bot.loop.run_until_complete(autobot())
-
+LOGS.info(
+    f"Jika Anda Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/AbingSupport")
+LOGS.info(
+    f"⚡AbingxUserbot⚡ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+idle()
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
