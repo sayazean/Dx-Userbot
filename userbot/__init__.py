67bfb76c03ef4905eca9c83d198cb5ee03e5f403
ABING <nabilrmdaan3@gmail.com>
2022-05-07 08:46:18 +0000

    pylint: auto fixes
    Signed-off-by: ABING <nabilrmdaan3@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -191,8 +191,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
-    "UPSTREAM_REPO_URL",
-    "https://github.com/sayazean/Dx-Userbot)
+    "UPSTREAM_REPO_URL", "https: // github.com / sayazean / Dx - Userbot)
 UPSTREAM_REPO_BRANCH = os.environ.get(
     "UPSTREAM_REPO_BRANCH", "Dx-Userbot")
 
