3651f2a0815f20d41f4c1ea59481d1286456b55f
sayazean <105099566+sayazean@users.noreply.github.com>
2022-05-07 17:39:39 +0700

    Update total_message.py

--- userbot/modules/total_message.py
+++ userbot/modules/total_message.py
@@ -1,10 +1,10 @@
-from userbot.utils import edit_or_reply, bing_cmd
+from userbot.utils import edit_or_reply, zean_cmd
 from userbot import CMD_HELP, bot, CMD_HANDLER as cmd
 
 
 # Port By @VckyouuBitch From GeezProject
 # Untuk Siapapun Yang Hapus Credits Ini, Kamu Anjing:)
-@bing_cmd(pattern="tmsg (.*)")
+@zean_cmd(pattern="tmsg (.*)")
 async def _(event):
     k = await event.get_reply_message()
     if k:
