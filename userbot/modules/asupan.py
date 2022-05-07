143098049b1b71b2db231d715948d78ed82f336f
sayazean <105099566+sayazean@users.noreply.github.com>
2022-05-07 16:17:36 +0700

    Update asupan.py

--- userbot/modules/asupan.py
+++ userbot/modules/asupan.py
@@ -4,13 +4,13 @@
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
-from userbot.utils import bing_cmd
+from userbot.utils import zean_cmd
 import random
 from userbot import owner
 from telethon.tl.types import InputMessagesFilterVideo
 
 
-@bing_cmd(pattern="asupan$")
+@zean_cmd(pattern="asupan$")
 async def _(event):
     try:
         asupannya = [
