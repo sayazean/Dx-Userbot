ee68880f7b3e8c2bb06cfdb9214f80d4de94cf92
sayazean <105099566+sayazean@users.noreply.github.com>
2022-05-07 15:31:35 +0700

    Update start.py

--- userbot/utils/start.py
+++ userbot/utils/start.py
@@ -12,8 +12,8 @@ async def startupmessage():
             await tgbot.send_file(
                 BOTLOG_CHATID,
                 "https://telegra.ph/file/8dad04830561de07c35d7.jpg",
-                caption="⚡ **AbingxUserbot Berhasil Diaktifkan**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 8.0@AbingxUserbot\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @AbingProject ",
-                buttons=[(Button.url("ꜱᴜᴘᴘᴏʀᴛ", "https://t.me/AbingSupport"),)],
+                caption="⚡ **Dx-Userbot Berhasil Diaktifkan**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 8.0@Dx-Userbot\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @ZeannProject ",
+                buttons=[(Button.url("ꜱᴜᴘᴘᴏʀᴛ", "https://t.me/ZeanSupport"),)],
             )
     except Exception as e:
         LOGS.error(e)
