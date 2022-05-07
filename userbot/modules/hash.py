581875fda8b393b34fcf93992c5c15f351ccde16
sayazean <105099566+sayazean@users.noreply.github.com>
2022-05-07 16:53:01 +0700

    Update hash.py

--- userbot/modules/hash.py
+++ userbot/modules/hash.py
@@ -10,10 +10,10 @@ from subprocess import run as runapp
 import pybase64
 
 from userbot import CMD_HELP, CMD_HANDLER as cmd
-from userbot.utils import bing_cmd
+from userbot.utils import zean_cmd
 
 
-@bing_cmd(pattern="hash (.*)")
+@zean_cmd(pattern="hash (.*)")
 async def gethash(hash_q):
     """ For .hash command, find the md5, sha1, sha256, sha512 of the string. """
     hashtxt_ = hash_q.pattern_match.group(1)
@@ -57,7 +57,7 @@ async def gethash(hash_q):
         await hash_q.reply(ans)
 
 
-@bing_cmd(pattern="base64 (en|de) (.*)")
+@zean_cmd(pattern="base64 (en|de) (.*)")
 async def endecrypt(query):
     """ For .base64 command, find the base64 encoding of the given string. """
     if query.pattern_match.group(1) == "en":
