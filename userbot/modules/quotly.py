54296f38839497e6adaa2347201f4966737d45bd
sayazean <105099566+sayazean@users.noreply.github.com>
2022-05-07 17:24:16 +0700

    Update quotly.py

--- userbot/modules/quotly.py
+++ userbot/modules/quotly.py
@@ -9,13 +9,13 @@ from telethon.tl.functions.users import GetFullUserRequest
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
-from userbot.utils import edit_delete, edit_or_reply, bing_cmd
+from userbot.utils import edit_delete, edit_or_reply, zean_cmd
 from userbot.utils.misc import create_quotly
 
 from .carbon import all_col
 
 
-@bing_cmd(pattern="q( (.*)|$)")
+@zean_cmd(pattern="q( (.*)|$)")
 async def quotly(event):
     match = event.pattern_match.group(1).strip()
     if not event.is_reply:
@@ -70,7 +70,7 @@ async def quotly(event):
         file = await create_quotly(reply_, bg=match, reply=replied_to, sender=user)
     except Exception as er:
         return await msg.edit(f"**ERROR:** `{er}`")
-    message = await reply.reply("Quotly by AbingxUserbot", file=file)
+    message = await reply.reply("Quotly by Dx-Userbot", file=file)
     remove(file)
     await msg.delete()
     return message
