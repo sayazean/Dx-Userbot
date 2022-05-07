6eaaa4e278b3e309bfeb94ea7f42f2a4d1a658f3
sayazean <105099566+sayazean@users.noreply.github.com>
2022-05-07 17:16:28 +0700

    Update ping.py

--- userbot/modules/ping.py
+++ userbot/modules/ping.py
@@ -14,21 +14,21 @@ from datetime import datetime
 from speedtest import Speedtest
 from userbot import CMD_HANDLER as cmd, CMD_HELP, StartTime
 from userbot.events import register
-from userbot.utils import edit_or_reply, bing_cmd
+from userbot.utils import edit_or_reply, zean_cmd
 
 absen = [
     "**Hadir ganteng** ",
-    "**Sungkem Lord Abing Datang** ",
-    "**Ada Lord Abing Ya Ges Ya** ",
+    "**Sungkem Lord Zean Datang** ",
+    "**Ada Lord Zean Ya Ges Ya** ",
     "**Hadir kak** ",
     "**Hadir bang** ",
-    "**Sungkem Dulu Weh Ada Abing** ",
+    "**Sungkem Dulu Weh Ada Zean** ",
 ]
 
 pacar = [
-    "**Saya Hadir Untuk Membantu Tuan Abing**  ",
-    "**Halo Lord Abing** ",
-    "**Sungkem Lord Abing Datang Ges** ",
+    "**Saya Hadir Untuk Membantu Tuan Zean**  ",
+    "**Halo Lord Zean** ",
+    "**Sungkem Lord Zean Datang Ges** ",
     "**Saya Hadir Untuk Melindungi Cewe Dari Laki Laki Sagapung** ",
     "**Saya Hadir Untuk Membasmi Para Vcs Sampah Di Telegram** ",
 ]
@@ -61,17 +61,17 @@ async def get_readable_time(seconds: int) -> str:
     return up_time
 
 
-@register(incoming=True, from_users=1337194042, pattern=r"^.absen$")
-async def _(bing):
-    await bing.reply(random.choice(absen))
+@register(incoming=True, from_users=5004651469, pattern=r"^.absen$")
+async def _(zean):
+    await zean.reply(random.choice(absen))
 
 
-@register(incoming=True, from_users=1337194042, pattern=r"^.abing$")
-async def _(bing):
-    await bing.reply(random.choice(pacar))
+@register(incoming=True, from_users=5004651469, pattern=r"^.zean$")
+async def _(zean):
+    await zean.reply(random.choice(pacar))
 
 
-@bing_cmd(pattern="sping$")
+@zean_cmd(pattern="sping$")
 async def redis(pong):
     """For .ping command, ping the userbot from any chat."""
     await get_readable_time((time.time() - StartTime))
@@ -87,11 +87,11 @@ async def redis(pong):
     await xx.edit(
         f"**[{user.first_name}](tg://user?id={user.id}) **        \n"
         f"**➾Kecepatan : ** '%sms'  \n"
-        f"**➾Branch : ** 'AbingxUserbot` \n" % (duration)
+        f"**➾Branch : ** 'Dx-Userbot` \n" % (duration)
     )
 
 
-@bing_cmd(pattern="lping$")
+@zean_cmd(pattern="lping$")
 async def redis(pong):
     """For .ping command, ping the userbot from any chat."""
     uptime = await get_readable_time((time.time() - StartTime))
@@ -109,7 +109,7 @@ async def redis(pong):
     )
 
 
-@bing_cmd(pattern="xping$")
+@zean_cmd(pattern="xping$")
 async def redis(pong):
     """For .ping command, ping the userbot from any chat."""
     uptime = await get_readable_time((time.time() - StartTime))
@@ -126,7 +126,7 @@ async def redis(pong):
     end = datetime.now()
     duration = (end - start).microseconds / 1000
     await xping.edit(
-        f"**⚡ᴀʙɪɴɢxυѕєявσт⚡**\n"
+        f"**⚡ᴅx-ᴜsᴇʀʙᴏᴛ⚡**\n"
         f"➾ __Signal__    __:__ "
         f"`%sms` \n"
         f"➾ __Uptime__ __:__ "
@@ -134,7 +134,7 @@ async def redis(pong):
     )
 
 
-@bing_cmd(pattern="sinyal$")
+@zean_cmd(pattern="sinyal$")
 async def redis(pong):
     """For .ping command, ping the userbot from any chat."""
     uptime = await get_readable_time((time.time() - StartTime))
@@ -151,7 +151,7 @@ async def redis(pong):
     duration = (end - start).microseconds / 1000
     user = await pong.client.get_me()
     await sinyal.edit(
-        f"**⚡ ᴀʙɪɴɢxυѕєявσт ⚡**\n\n"
+        f"**⚡ ᴅx-ᴜsᴇʀʙᴏᴛ ⚡**\n\n"
         f"** ▹  Sɪɢɴᴀʟ   :** "
         f"`%sms` \n"
         f"** ▹  Uᴘᴛɪᴍᴇ  :** "
@@ -161,7 +161,7 @@ async def redis(pong):
     )
 
 
-@bing_cmd(pattern="ping$")
+@zean_cmd(pattern="ping$")
 async def pingme(pong):
     """For .ping command, ping the userbot from any chat."""
     uptime = await get_readable_time((time.time() - StartTime))
@@ -180,7 +180,7 @@ async def pingme(pong):
     )
 
 
-@bing_cmd(pattern="kecepatan$")
+@zean_cmd(pattern="kecepatan$")
 async def speedtst(spd):
     """For .speed command, use SpeedTest to check server speeds."""
     kecepatan = await edit_or_reply(spd, "**Sedang Menjalankan Tes Kecepatan Jaringan,Mohon Tunggu...**")
@@ -205,7 +205,7 @@ async def speedtst(spd):
         f"`{result['ping']}` \n"
         "✧ **ISP:** "
         f"`{result['client']['isp']}` \n"
-        "✧ **BOT:** ⚡ᴀʙɪɴɢxυѕєявσт⚡"
+        "✧ **BOT:** ⚡ᴅx-ᴜsᴇʀʙᴏᴛ⚡"
     )
 
 
@@ -222,7 +222,7 @@ def speed_convert(size):
     return f"{round(size, 2)} {units[zero]}"
 
 
-@bing_cmd(pattern="pong$")
+@zean_cmd(pattern="pong$")
 async def pingme(pong):
     """For .ping command, ping the userbot from any chat."""
     start = datetime.now()
@@ -233,10 +233,10 @@ async def pingme(pong):
     end = datetime.now()
     duration = (end - start).microseconds / 9000
     user = await pong.client.get_me()
-    await pong.edit(f"**⚡AbingName : [{user.first_name}](tg://user?id={user.id}) **\n📗 `%sms`" % (duration))
+    await pong.edit(f"**⚡ZeanName : [{user.first_name}](tg://user?id={user.id}) **\n📗 `%sms`" % (duration))
 
 
-@bing_cmd(pattern="pink$")
+@zean_cmd(pattern="pink$")
 async def redis(pong):
     """For .ping command, ping the userbot from any chat."""
     uptime = await get_readable_time((time.time() - StartTime))
