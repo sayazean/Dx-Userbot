fa1410149fedf582c7e992b87786fbbfe81c6266
sayazean <105099566+sayazean@users.noreply.github.com>
2022-05-07 15:51:12 +0700

    Update abinghelper.py

--- userbot/modules/abinghelper.py
+++ userbot/modules/abinghelper.py
@@ -1,28 +1,28 @@
 """ Userbot module for other small commands. """
 from userbot import CMD_HELP, owner, CMD_HANDLER as cmd
-from userbot.utils import edit_or_reply, bing_cmd
+from userbot.utils import edit_or_reply, zean_cmd
 
 
-@bing_cmd(pattern="lhelp$")
+@zean_cmd(pattern="lhelp$")
 async def usit(e):
     await edit_or_reply(e,
                         f"**Halo {owner} Jika Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `{cmd}help` Atau Bisa Minta Bantuan Ke:\n"
-                        "\n[Telegram](t.me/sayaabing)"
-                        "\n[Repo](https://github.com/S/AbingxUserbot)"
+                        "\n[Telegram](t.me/zeannihbos)"
+                        "\n[Repo](https://sayazean/Dx-Userbot)"
                         "\n[Instagram](instagram.com/aabinggg)")
 
 
-@bing_cmd(pattern="vars$")
+@zean_cmd(pattern="vars$")
 async def var(m):
     await edit_or_reply(m,
                         f"**Disini Daftar Vars Dari {owner}:**\n"
-                        "\n[DAFTAR VARS](https://raw.githubusercontent.com/SayaAbing/AbingxUserbot/AbingxUserbot/varshelper.txt)")
+                        "\n[DAFTAR VARS](https://raw.githubusercontent.com/sayazean/Dx-Userbot/Dx-Userbot/varshelper.txt)")
 
 
 CMD_HELP.update({
     "helper":
     f"`{cmd}lhelp`\
-\nUsage: Bantuan Untuk AbingxUserbot.\
+\nUsage: Bantuan Untuk Dx-Userbot.\
 \n`{cmd}vars`\
 \nUsage: Melihat Daftar Vars."
 })
