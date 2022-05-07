d636e0632f455784c9758a820900d23e4ea1ad6e
sayazean <105099566+sayazean@users.noreply.github.com>
2022-05-07 16:25:52 +0700

    Update detection.py

--- userbot/modules/detection.py
+++ userbot/modules/detection.py
@@ -1,9 +1,9 @@
-from userbot.utils import edit_or_reply, bing_cmd
+from userbot.utils import edit_or_reply, zean_cmd
 from userbot import CMD_HELP, bot, CMD_HANDLER as cmd
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
 
-@bing_cmd(pattern="detect(?: |$)(.*)")
+@zean_cmd(pattern="detect(?: |$)(.*)")
 async def detect(event):
     if event.fwd_from:
         return
