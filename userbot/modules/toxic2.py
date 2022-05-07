cec54b9988ec06437599c3dbb48ccc455184867c
sayazean <105099566+sayazean@users.noreply.github.com>
2022-05-07 17:41:38 +0700

    Update toxic2.py

--- userbot/modules/toxic2.py
+++ userbot/modules/toxic2.py
@@ -1,9 +1,9 @@
 from time import sleep
 from userbot import CMD_HELP, CMD_HANDLER as cmd
-from userbot.utils import edit_or_reply, bing_cmd
+from userbot.utils import edit_or_reply, zean_cmd
 
 
-@bing_cmd(pattern='ngentot(?: |$)(.*)')
+@zean_cmd(pattern='ngentot(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(1)
@@ -29,7 +29,7 @@ async def typewriter(typew):
 # Create by myself @localheart
 
 
-@bing_cmd(pattern='goblok(?: |$)(.*)')
+@zean_cmd(pattern='goblok(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(1)
@@ -55,7 +55,7 @@ async def typewriter(typew):
 # Create by myself @localheart
 
 
-@bing_cmd(pattern='ngatain(?: |$)(.*)')
+@zean_cmd(pattern='ngatain(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(1)
@@ -81,7 +81,7 @@ async def typewriter(typew):
 # Create by myself @localheart
 
 
-@bing_cmd(pattern='yatim(?: |$)(.*)')
+@zean_cmd(pattern='yatim(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(1)
@@ -107,7 +107,7 @@ async def typewriter(typew):
 # Create by myself @localheart
 
 
-@bing_cmd(pattern="kont(?: |$)(.*)")
+@zean_cmd(pattern="kont(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(1)
