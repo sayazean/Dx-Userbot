50e98ee1bdf6e0af7fd1e707b6e131bd36a392c0
sayazean <105099566+sayazean@users.noreply.github.com>
2022-05-07 17:57:02 +0700

    Update tools.py

--- userbot/utils/tools.py
+++ userbot/utils/tools.py
@@ -188,8 +188,8 @@ async def bash(cmd):
 
 def post_to_telegraph(title, html_format_content):
     post_client = TelegraphPoster(use_api=True)
-    auth_name = "AbingxUserbot"
-    auth_url = "https://github.com/SayaAbing/AbingxUserbot"
+    auth_name = "Dx-Userbot"
+    auth_url = "https://github.com/sayazean/Dx-Userbot"
     post_client.create_api_token(auth_name)
     post_page = post_client.post(
         title=title,
