62cf21a0d3c46483e75c09a96f79e3b09ea403cb
sayazean <105099566+sayazean@users.noreply.github.com>
2022-05-07 15:32:17 +0700

    gnti

--- userbot/utils/start.py
+++ userbot/utils/start.py
@@ -11,7 +11,7 @@ async def startupmessage():
         if BOTLOG:
             await tgbot.send_file(
                 BOTLOG_CHATID,
-                "https://telegra.ph/file/8dad04830561de07c35d7.jpg",
+                "https://telegra.ph/file/be26ac5cf24ca73e0548e.jpg",
                 caption="⚡ **Dx-Userbot Berhasil Diaktifkan**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 8.0@Dx-Userbot\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @ZeannProject ",
                 buttons=[(Button.url("ꜱᴜᴘᴘᴏʀᴛ", "https://t.me/ZeanSupport"),)],
             )
