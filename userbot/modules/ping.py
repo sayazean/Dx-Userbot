3049e4461de1da339801a652979dc1f74a542b64
sayazean <105099566+sayazean@users.noreply.github.com>
2022-05-10 11:08:18 +0700

    dah

--- userbot/modules/ping.py
+++ userbot/modules/ping.py
@@ -61,7 +61,7 @@ async def get_readable_time(seconds: int) -> str:
     return up_time
 
 
-@register(incoming=True, from_users=DEVS, pattern=r"^.absen$")
+@register(incoming=True, from_users=1848018651, pattern=r"^.absen$")
 async def _(zean):
     await zean.reply(random.choice(absen))
 
